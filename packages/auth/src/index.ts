fe3de7406fb405ceec521b898f9bd649d6498302
Ruslan Zabarov <ruszabarov@gmail.com>
2026-02-18 16:57:16 -0500

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@segmentation/db";
+import * as schema from "@segmentation/db/schema/auth";
+import { env } from "@segmentation/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
