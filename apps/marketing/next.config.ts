997a4883e05f0940949db8024942e001cea339c6
Ruslan Zabarov <ruszabarov@gmail.com>
2026-02-20 22:05:05 -0500

    marketing/fix: adjust next config

--- apps/marketing/next.config.ts
+++ apps/marketing/next.config.ts
@@ -5,9 +5,6 @@ const nextConfig: NextConfig = {
   typedRoutes: true,
   reactCompiler: true,
   cacheComponents: true,
-  images: {
-    remotePatterns: [new URL("https://d2l0k5nmpb02cr.cloudfront.net/*/**")],
-  },
 };
 
 export default nextConfig;
