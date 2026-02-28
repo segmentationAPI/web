9b44d78e4a636ce68c94489d25e5f3212d720c31
Ruslan Zabarov <ruszabarov@gmail.com>
2026-02-28 00:33:10 -0500

    fix: clean up auto-label
    GitOrigin-RevId: 3c8ab759d15a48f1f1e35e6acf12e629148c8a18

--- apps/dashboard/src/app/auto-label/[projectId]/_components/project-dashboard-wrapper.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { ProjectDashboard } from "./project-dashboard";
-
-export function ProjectDashboardClientWrapper({ projectId }: { projectId: string }) {
-    return <ProjectDashboard projectId={projectId} />;
-}
